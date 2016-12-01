b8710c9df96fbade5d381b5f193ce5f7fed99fb1
Davi Chaves <dchaves@usc.edu>
2016-11-30 23:00:29 -0800

    fixing cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-const cors = require('koa-cors');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var comics = require('./routes/comics');
